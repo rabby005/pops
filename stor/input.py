2c49438664964d3fadadbe7d2df0bcc816356a63
rabby005 <farabyabdulla@gmail.com>
2025-12-24 13:56:58 +0600

    input

--- stor/input.py
+++ stor/input.py
@@ -2,8 +2,16 @@
 # input("enter your name:")
 
 
-name = input("enter your name :")
-print("welcome",name)
+# name = input("enter your name :")
+# print("welcome",name)
 
+# age = input("entuer your age:")
+# print("Your age is:",age)
+
+
+
+name = input("enter your name :")
 age = input("entuer your age:")
+
+print("welcome",name)
 print("Your age is:",age)
