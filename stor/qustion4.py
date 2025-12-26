8133b2810ed947d1da9a1ed23a3f3d1c19ec414a
rabby005 <farabyabdulla@gmail.com>
2025-12-26 15:44:26 +0600

    qustion4

---
+++ stor/qustion4.py
@@ -0,0 +1,10 @@
+
+
+#WAP to input 2 int numbers, a and b.
+#Print True if a is greater than or equal to b. If not print False.
+
+a = int(input("enter first: "))
+b = int(input("enter second : "))
+
+
+print (a >= b)
